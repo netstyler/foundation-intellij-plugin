a3e8007f03e7ce3e1ed95ab32945e4c0af8cd23b
GitHub Action <action@github.com>
2022-10-08 20:20:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/netstyler/foundationintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/netstyler/foundationintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.netstyler.foundationintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/netstyler/foundationintellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/netstyler/foundationintellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.netstyler.foundationintellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.netstyler.foundationintellijplugin.MyBundle
 
 class MyApplicationService {
 
